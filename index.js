9e2f9d0ace6cd0e4b2764ebe962ffdf8f300747e
Quoc Dung <pinterest2110@gmail.com>
2021-10-15 16:28:41 +0700

    update

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const switchRegister = document.getElementById('user_switch-btn-register')
-
+const switchLogin = document.getElementById('user_switch-btn-login')
+const paginationItem = document.querySelectorAll('.pagination-item')
 switchRegister.onclick = function () {
     document.getElementById('loginModalLog').style.display = 'none'
     document.getElementById('loginModal').style.display = 'block'
 }
 
 
-const switchLogin = document.getElementById('user_switch-btn-login')
+
 
 switchLogin.onclick = function () {
     document.getElementById('loginModal').style.display = 'none'
     document.getElementById('loginModalLog').style.display = 'block'
+}
+
+
+for (var i = 0; i < paginationItem.length; ++i) {
+    paginationItem[i].onclick = function (e) {
+        e.preventDefault()
+        // paginationItem[6].
+        document.querySelector('.pagination-item.pagination-item--active').classList.remove("pagination-item--active")
+        this.className = 'pagination-item--active pagination-item'
+    }
 }
 
